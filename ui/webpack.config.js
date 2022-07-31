3a38045fa084ce28de8876af1031323f59faeeda
abhishek1993c <abhishek1993c@gmail.com>
2022-07-31 17:52:59 +0530

    webpack changes

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.jsx'),
+    entry: path.resolve(__dirname, 'src/index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx','.tsx']
+        extensions: ['.js', '.jsx','.ts','.tsx']
       }
 };
