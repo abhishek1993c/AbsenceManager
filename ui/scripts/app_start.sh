384e952d1db674259105fceb5131136a49004cdc
abhishek1993c <abhishek1993c@gmail.com>
2022-07-31 18:52:23 +0530

    startup script update

--- ui/scripts/app_start.sh
+++ ui/scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/server/src
 npm start
-pm2 start npm --name "absenceManager" -- start
+pm2 serve --name "absenceManager" -s build
 pm2 startup
 pm2 save
 pm2 restart all
