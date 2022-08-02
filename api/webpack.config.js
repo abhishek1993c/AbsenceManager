260c9913dae188ba1ce3d85b6c59a66ee20fb1ca
abhishek1993c <abhishek1993c@gmail.com>
2022-08-02 20:56:04 +0530

    webpack changes

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-    entry: './index.ts',
+    entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'api.bundle.js'
