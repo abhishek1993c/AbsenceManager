49e806a82b2b1d4e138161b95d09741be4d9c2d3
abhishek1993c <abhishek1993c@gmail.com>
2022-08-02 19:58:12 +0530

    script test

--- api/scripts/app_start.sh
+++ api/scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/ec2-user/apiserver/src
+cd /home/ec2-user/apiserver/src/dist
 npm start
-pm2 serve --name "absenceManager" -s build
+pm2 start index.js --name "absenceManager"
 pm2 startup
 pm2 save
 pm2 restart all
