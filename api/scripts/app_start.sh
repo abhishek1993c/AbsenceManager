0161b21492aced426279e0aef529c97e18391db4
abhishek1993c <abhishek1993c@gmail.com>
2022-08-02 20:47:07 +0530

    config update

--- api/scripts/app_start.sh
+++ api/scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/apiserver/src
 npm start
-pm2 start index.js --name "absenceManager"
+pm2 start dist/index.js --name "absenceManager" --watch -i max
 pm2 startup
 pm2 save
 pm2 restart all
