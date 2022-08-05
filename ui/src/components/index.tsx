f26f6afba5ad5bfce6be69752806d5ab0c4c1efe
abhishek1993c <abhishek1993c@gmail.com>
2022-08-05 20:11:12 +0530

    FIxed eslint errors

--- ui/src/components/index.tsx
+++ ui/src/components/index.tsx
@@ -1,17 +1,17 @@
-import React, {FC} from 'react';
-import {Routes, Route} from 'react-router-dom';
+import React, { FC } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Navigate } from 'react-router';
 
 import { ROUTES } from '../constants';
 import AbsenceLanding from './AbsenceLanding';
 
 const AbsenceManagerRoot: FC = () => {
-    return (
-        <Routes>
-            <Route path={ROUTES.ABSENCE_LANDING} element={<AbsenceLanding/>}/>
-            <Route path={ROUTES.ROOT} element={<Navigate to={ROUTES.ABSENCE_LANDING} />}/>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path={ROUTES.ABSENCE_LANDING} element={<AbsenceLanding/>}/>
+      <Route path={ROUTES.ROOT} element={<Navigate to={ROUTES.ABSENCE_LANDING} />}/>
+    </Routes>
+  )
 }
 
 export default AbsenceManagerRoot;
